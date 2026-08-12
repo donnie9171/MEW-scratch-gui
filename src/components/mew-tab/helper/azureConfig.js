40da584d03ca540a51d9d15577a9a1fd5c9f61b5
Enoch Kang <enochkang9171@gmail.com>
2026-08-12 17:37:42 +0800

    fix: update azure source config

--- src/components/mew-tab/helper/azureConfig.js
+++ src/components/mew-tab/helper/azureConfig.js
@@ -1,3 +1,3 @@
-const AZURE_SOURCE = 'https://multiagent-exploration-workbench-c8htdxerg6d9faa0.uksouth-01.azurewebsites.net';
+const AZURE_SOURCE = 'mewapiproxy-f7hvb2fufphra5bm.uaenorth-01.azurewebsites.net';
 
 export {AZURE_SOURCE};
